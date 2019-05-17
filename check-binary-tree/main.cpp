96120fbd4aa3481e4653d1967fdbcd2258697a79
valery <8alery@gmail.com>
2019-05-17 20:20:39 +0300

    check-binary-tree

---
+++ check-binary-tree/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+struct Node {
+    int key{0};
+    Node* left{nullptr};
+    Node* right{nullptr};
+};
+
+bool traverse_tree(Node &node){
+    std::stack<Node*> stack;
+
+    Node *current = &node;
+
+    bool correct = true;
+    int previous = INT32_MIN;
+
+    while (current != nullptr || !stack.empty()){
+        if (!stack.empty()){
+            current = stack.top();
+            stack.pop();
+
+            if (current->key < previous){
+                correct = false;
+                break;
+            }
+            previous = current->key;
+
+            if (current->right) {
+                current = current->right;
+            } else {
+                current = nullptr;
+            }
+        }
+        while (current!=nullptr){
+            stack.push(current);
+            current=current->left;
+        }
+    }
+
+    return correct;
+
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<Node> nodes(n);
+    for (auto& n: nodes) {
+        int leftIdx, rightIdx;
+        std::cin >> n.key >> leftIdx >> rightIdx;
+        if (leftIdx != -1)
+            n.left = &nodes[leftIdx];
+        if (rightIdx != -1)
+            n.right = &nodes[rightIdx];
+    }
+
+    bool isCorrect = nodes.empty() ? true : traverse_tree(nodes.front());
+    std::cout << (isCorrect ? "CORRECT": "INCORRECT");
+
+
+    return 0;
+}
--- traverse-tree/cmake-build-debug/CMakeFiles/traverse_tree.dir/DependInfo.cmake
+++ traverse-tree/cmake-build-debug/CMakeFiles/traverse_tree.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/valery/CLionProjects/algorithms/traverse-tree/main.cpp" "/home/valery/CLionProjects/algorithms/traverse-tree/cmake-build-debug/CMakeFiles/traverse_tree.dir/main.cpp.o"
+  "/Users/valery/LearningProjects/algorithms/traverse-tree/main.cpp" "/Users/valery/LearningProjects/algorithms/traverse-tree/cmake-build-debug/CMakeFiles/traverse_tree.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
